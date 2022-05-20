2b446d60e3b985a750392f26deb0f9d4f43d1177
Clover Dev <cloverdev88@gmail.com>
2022-05-20 14:04:37 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withTM = require("next-transpile-modules")([
+  "lodash-es",
+  "react-d3-speedometer"
+]);
+
+module.exports = withTM();
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
